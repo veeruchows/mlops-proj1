9c3d9b7f4489ce7e0b28a3d0716b587e630f9a68
Veeru <singuveeru101@gmail.com>
2024-12-20 18:18:08 -0500

    project env ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Veeru",
+    author_email="singuveeru101@gmail.com",
+    packages=find_packages()
+)
